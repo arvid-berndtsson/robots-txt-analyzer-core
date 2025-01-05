6f281d5748acf05d46f27b88010f2362c95b4aeb
Arvid Berndtsson <103070833+arvid-berndtsson@users.noreply.github.com>
2025-01-05 16:30:09 +0100

    fix(typing): add missing types (#8)

--- src/routes/api/v1/history/index.tsx
+++ src/routes/api/v1/history/index.tsx
@@ -1,17 +1,26 @@
 import { type RequestHandler } from '@builder.io/qwik-city';
 
-export const onGet: RequestHandler = async ({ json, env }) => {
-  if (!env.HISTORY_KV) {
+export const onGet: RequestHandler = async ({ json, env, request }) => {
+  const apiKey = env.get("API_KEY");
+  const historyKV = env.get("HISTORY_KV");
+
+  if (request.headers.get("X-API-Key") !== apiKey) {
+    throw json(401, { error: "Unauthorized" });
+  }
+  if (!historyKV) {
     throw json(500, { error: 'History storage not available' });
   }
 
-  const list = await env.HISTORY_KV.list({ prefix: 'analysis:' });
-  const history = await Promise.all(
-    list.keys.map(async (key) => {
-      const value = await env.HISTORY_KV.get(key.name);
-      return JSON.parse(value);
-    })
-  );
+  json(501, { error: 'Not implemented' });
+
+  // TODO: Implement saving results functionality
+  // const list = await historyKV.list({ prefix: 'analysis:' });
+  // const history = await Promise.all(
+  //   list.keys.map(async (key) => {
+  //     const value = await historyKV.get(key.name);
+  //     return JSON.parse(value);
+  //   })
+  // );
 
   json(200, history);
 };
--- src/routes/api/v1/results/[domain]/index.tsx
+++ src/routes/api/v1/results/[domain]/index.tsx
@@ -1,16 +1,27 @@
 import { type RequestHandler } from '@builder.io/qwik-city';
 
 export const onGet: RequestHandler = async ({ params, json, env }) => {
+  json(501, { error: 'Not implemented' });
+  
+
+    // TODO: Implement saving results functionality
   const { domain } = params;
 
-  if (!env.HISTORY_KV) {
+  console.log(domain);
+    
+  const historyKV = env.get("HISTORY_KV");
+
+  if (!historyKV) {
     throw json(500, { error: 'Analysis storage not available' });
   }
 
-  const analysis = await env.HISTORY_KV.get(`analysis:${domain}`);
-  if (!analysis) {
-    throw json(404, { error: 'Analysis not found for the specified domain' });
-  }
 
-  json(200, JSON.parse(analysis));
+
+
+  // const analysis = await env.HISTORY_KV.get(`analysis:${domain}`);
+  // if (!analysis) {
+  //   throw json(404, { error: 'Analysis not found for the specified domain' });
+  // }
+
+  // json(200, JSON.parse(analysis));
 };
